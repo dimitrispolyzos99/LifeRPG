b3a93788292bdbcfe50e807408395162bf0ab63b
Dimitrispol99 <dimitrispoluzos99@hotmail.com>
2026-05-30 14:37:48 +0300

    Refactor: Extract BattleLogService with LogService protocol

---
+++ LifeRPG/ViewModel/BattleLogService.swift
@@ -0,0 +1,8 @@
+//
+//  BattleLogService.swift
+//  LifeRPG
+//
+//  Created by Dimitris Poluzos on 23/5/26.
+//
+
+import Foundation
---
+++ LifeRPG/ViewModel/SaveService.swift
@@ -0,0 +1,8 @@
+//
+//  SaveService.swift
+//  LifeRPG
+//
+//  Created by Dimitris Poluzos on 30/5/26.
+//
+
+import Foundation
